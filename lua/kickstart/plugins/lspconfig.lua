310187a49dc64e9137c5d33e9505e667470e1a55
Hatvani Tamás <hatvanitamas09@gmail.com>
2026-09-06 11:23:30 +0200

    added emmet language server to lspconfig

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -236,6 +236,27 @@ return {
             },
           },
         },
+        -- Add Emmet here:
+        emmet_language_server = {
+          filetypes = {
+            'css',
+            'eruby',
+            'html',
+            'javascript',
+            'javascriptreact',
+            'less',
+            'pug',
+            'sass',
+            'scss',
+            'typescriptreact',
+          },
+          init_options = {
+            showAbbreviationSuggestions = true,
+            showExpandedAbbreviation = 'always',
+            -- Enabled so blink.cmp can handle tabstops/placeholders
+            showSuggestionsAsSnippets = true,
+          },
+        },
       }
 
       require('java').setup {}
@@ -249,37 +270,9 @@ return {
       -- allow for server-specific overrides.
       for server_name, server_config in pairs(servers) do
         server_config.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server_config.capabilities or {})
-        --require('lspconfig')[server_name].setup(server_config)
         vim.lsp.config(server_name, server_config)
+        vim.lsp.enable(server_name)
       end
-
-      vim.lsp.config['emmet-language-server'] = {
-        opts = {
-          filetypes = { 'css', 'eruby', 'html', 'javascript', 'javascriptreact', 'less', 'sass', 'scss', 'pug', 'typescriptreact' },
-          -- Read more about this options in the [vscode docs](https://code.visualstudio.com/docs/editor/emmet#_emmet-configuration).
-          -- **Note:** only the options listed in the table are supported.
-          init_options = {
-            ---@type table<string, string>
-            includeLanguages = {},
-            --- @type string[]
-            excludeLanguages = {},
-            --- @type string[]
-            extensionsPath = {},
-            --- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/preferences/)
-            preferences = {},
-            --- @type boolean Defaults to `true`
-            showAbbreviationSuggestions = true,
-            --- @type "always" | "never" Defaults to `"always"`
-            showExpandedAbbreviation = 'always',
-            --- @type boolean Defaults to `false`
-            showSuggestionsAsSnippets = false,
-            --- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/syntax-profiles/)
-            syntaxProfiles = {},
-            --- @type table<string, string> [Emmet Docs](https://docs.emmet.io/customization/snippets/#variables)
-            variables = {},
-          },
-        },
-      }
 
       -- Ensure the servers and tools above are installed
       --
